9f73a18b37e87e708f644e12ce4447cc58ca02d0
sdfsdf <aramaraqelyan503@gmail.com>
2022-03-29 00:02:05 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,61 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+//              Task - 1            //
+
+        Integer[] array = {1,2,3,4,5,6,7,8,9};
+        System.out.println(isContainDuplicate(array));
+//              Task - 2            //
+
+        Integer[] array1 = {1,2,3,4,5,6,6,7,7,8,9};
+        System.out.println(onlyUnics(array1));
+//              Task - 3            //
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a String : ");
+        String str = scanner.next();
+        printCountOfDuplications(str);
+
+    }
+
+    private static void printCountOfDuplications(String str) {
+        int size = str.length();
+        int count = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < size; i++){
+            if (map.containsKey(str.charAt(i))){
+                map.put(str.charAt(i), 2);
+                continue;
+            }
+            map.put(str.charAt(i), 1);
+        }
+        for (Map.Entry<Character, Integer> entry : map.entrySet()){
+            if (entry.getValue() == 2) {
+                count++;
+            }
+        }
+        System.out.println("Count of duplications is equal to : " + count);
+    }
+
+    private static <Integer> Set<Integer> onlyUnics(Integer[] array1) {
+        Set<Integer> numbers = new HashSet<>();
+        for (Integer integer : array1){
+            if (!numbers.add(integer)){
+                numbers.remove(integer);
+            }
+        }
+        return numbers;
+    }
+
+    private static <Integer> String isContainDuplicate(Integer[] array) {
+        Set<Integer> num = new HashSet<>();
+        for (Integer i : array){
+            if (!num.add(i)){
+                return "Array is contains duplicates.";
+            }
+        }
+        return "Array is not contains duplicates.";
+    }
+}
